d5c056c68e42996e91b02539e7ea2a03ff8df58d
Mark Wales <mark@smallhadroncollider.com>
2020-02-17 10:08:20 +0000

    docs: updates Stack resolver

--- src/Game.hs
+++ src/Game.hs
@@ -1,27 +1,31 @@
-module Game (play) where
+module Game
+  ( play
+  ) where
 
-import ClassyPrelude
+import           ClassyPrelude
 
-import Control.Lens ((&), (.~), (^.))
-import System.Random (getStdRandom, randomR)
+import           Control.Lens  ((&), (.~), (^.))
+import           System.Random (getStdRandom, randomR)
 
-import Brick
-import Brick.BChan (newBChan)
-import Graphics.Vty (Event(EvKey), Key(KChar, KEnter), mkVty, defaultConfig)
+import           Brick
+import           Brick.BChan   (newBChan)
+import           Graphics.Vty  (Event (EvKey), Key (KChar, KEnter),
+                                defaultConfig, mkVty)
 
-import qualified Actions (restart, jump, frame)
-import Attr (attr)
-import Draw (draw)
-import Loop (loop)
-import Types (Name, UI, Tick(Tick), create, speed, dimensions)
-import Window (getDimensions)
+import qualified Actions       (frame, jump, restart)
+import           Attr          (attr)
+import           Draw          (draw)
+import           Loop          (loop)
+import           Types         (Name, Tick (Tick), UI, create, dimensions,
+                                speed)
+import           Window        (getDimensions)
 
 handleTick :: UI -> EventM Name (Next UI)
 handleTick ui = do
-    let sp = ui ^. speed
-    s <- liftIO getDimensions
-    r <- liftIO $ getStdRandom (randomR (sp, sp * 3))
-    continue $ Actions.frame r (ui & dimensions .~ s)
+  let sp = ui ^. speed
+  s <- liftIO getDimensions
+  r <- liftIO $ getStdRandom (randomR (sp, sp * 3))
+  continue $ Actions.frame r (ui & dimensions .~ s)
 
 handleEvent :: UI -> BrickEvent Name Tick -> EventM Name (Next UI)
 handleEvent ui (VtyEvent (EvKey (KChar 'q') [])) = halt ui
@@ -31,26 +35,29 @@ handleEvent ui (AppEvent Tick) = handleTick ui
 handleEvent ui _ = continue ui
 
 app :: App UI Tick Name
-app = App {
-    appDraw = draw
+app =
+  App
+  { appDraw = draw
   , appChooseCursor = neverShowCursor
   , appHandleEvent = handleEvent
   , appStartEvent = return
   , appAttrMap = const attr
-}
+  }
 
 getSpeed :: IO Int
 getSpeed = do
-    args <- getArgs
-    return $ case args of
-        [sp] -> fromMaybe 10 $ readMay sp
-        _ -> 10
-
+  args <- getArgs
+  return $
+    case args of
+      [sp] -> fromMaybe 10 $ readMay sp
+      _    -> 10
 
 play :: IO ()
 play = do
-    chan <- newBChan 1
-    loop chan
-    s <- getDimensions
-    sp <- getSpeed
-    void $ customMain (mkVty defaultConfig) (Just chan) app $ create s sp
+  chan <- newBChan 1
+  loop chan
+  s <- getDimensions
+  sp <- getSpeed
+  let builder = mkVty defaultConfig
+  initialVty <- builder
+  void $ customMain initialVty builder (Just chan) app $ create s sp
